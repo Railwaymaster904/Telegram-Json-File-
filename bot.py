1669794a1e25630f6d68da898780ef8dc92f51ec
Railwaymaster904 <railwaymaster904@gmail.com>
2026-08-19 02:26:47 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,11 +118,11 @@ def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("search", search))
-    app.add_handler(MessageHandler(filters.TEXT & \~filters.COMMAND, select_video))
+app.add_handler(CommandHandler("search", search))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, select_video))
 
-    print("Bot চালু...")
-    app.run_polling()
+print("Bot চালু...")
+app.run_polling()
 
 if __name__ == "__main__":
     main()
